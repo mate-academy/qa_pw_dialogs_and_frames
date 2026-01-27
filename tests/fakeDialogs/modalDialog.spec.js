f70ecb6915b6f9b4ac187764c1a9796c55ef2b6d
Nadiia Bohdan <nadiia.bohdan@holywater.tech>
2026-01-27 12:17:07 +0200

    Solution

--- tests/fakeDialogs/modalDialog.spec.js
+++ tests/fakeDialogs/modalDialog.spec.js
@@ -1,14 +1,15 @@
-import { test } from '@playwright/test';
+import { test, expect } from '@playwright/test';
 
-test('Fake modal dialog can be opened and closed', async ({}) => {
-  /*
-  Test:
-  1. Open the page
-   https://testpages.eviltester.com/styled/alerts/fake-alert-test.html
-  2. Click the button 'Show fake alert box'
-  3. Assert the dialog is opened
-  4. Assert the dialog message
-  5. Click the button 'Ok'
-  6. Assert the dialog was closed
-  */
+test('Fake modal dialog can be opened and closed', async ({ page }) => {
+  await page.goto('https://testpages.eviltester.com/styled/alerts/fake-alert-test.html');
+  await page.getByRole('button', { name: 'Show fake alert box' }).click();
+
+  const dialog = page.getByRole('dialog');
+
+  await expect(dialog).toBeVisible();
+  await expect(dialog).toContainText('I am a fake alert box!');
+
+  await dialog.getByRole('button', { name: 'OK' }).click();
+
+  await expect(dialog).toBeHidden();
 });
--- tests/frame/frameProductsList.spec.js
+++ tests/frame/frameProductsList.spec.js
@@ -1,16 +1,16 @@
-import { test } from '@playwright/test';
+import { test, expect } from '@playwright/test';
 
-test('Products list can be opened within the frame', async ({}) => {
-  /*
-  Test:
-  1. Open the page
-   https://webdriveruniversity.com/IFrame/index.html
-  2. Click 'Our Products' link
-  3. Assert that all the block links are visible
-   ('Specifal offers',  'Cameras', etc.) 
+test('Products list can be opened within the frame', async ({ page }) => {
+  await page.goto('https://webdriveruniversity.com/IFrame/index.html');
 
-  Tip:
-  Remember, that you need firstly to define the frame locator, 
-  and then find other elements within it. 
-  */
+  const frameLocator = page.frameLocator('iframe');
+  await frameLocator.getByRole('link', { name: 'Our Products' }).click();
+
+  await expect(frameLocator.getByRole('link', { name: 'Special Offers' })).toBeVisible();
+  await expect(frameLocator.getByRole('link', { name: 'Cameras' })).toBeVisible();
+  await expect(frameLocator.getByRole('link', { name: 'New Laptops' })).toBeVisible();
+  await expect(frameLocator.getByRole('link', { name: 'Used Laptops' })).toBeVisible();
+  await expect(frameLocator.getByRole('link', { name: 'Game Consoles' })).toBeVisible();
+  await expect(frameLocator.getByRole('link', { name: 'Components' })).toBeVisible();
+  await expect(frameLocator.getByRole('link', { name: 'Desktop Systems' })).toBeVisible();
 });
