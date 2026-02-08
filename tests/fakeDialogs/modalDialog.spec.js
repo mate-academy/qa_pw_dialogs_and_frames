5224eba56f59358f33e4a7fa764fbe6e8c18059b
Oleksander Pyavchik <pyavchik@gmail.com>
2026-02-08 16:57:56 +0200

    add tests involving work with dialogs and a frame

--- tests/fakeDialogs/modalDialog.spec.js
+++ tests/fakeDialogs/modalDialog.spec.js
@@ -4,13 +4,13 @@ test('Fake modal dialog can be opened and closed', async ({page}) => {
   await page.goto(
     'https://testpages.eviltester.com/styled/alerts/fake-alert-test.html',
   );
-  await page.getByRole('button', { name: 'Show fake alert box' }).click();
+  await page.getByRole('button', { name: 'Show modal dialog' }).click();
   await expect(page.getByRole('dialog')).toBeVisible();
   await expect(
-    page.getByRole('heading', { name: 'I am a fake alert box!' }),
+    page.getByRole('heading', { name: 'I am a modal div!' }),
   ).toBeVisible();
   await page.getByRole('button', { name: 'OK' }).click();
   await expect(
-    page.getByRole('heading', { name: 'I am a fake alert box!' }),
+    page.getByRole('heading', { name: 'I am a modal div!' }),
   ).toBeHidden();
 });
