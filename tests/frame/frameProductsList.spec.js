be93260ca4f437de91a407c6245c09166cf6cb7f
Alyona <killer2.0.1al@gmail.com>
2026-06-23 19:19:37 +0300

    solved

--- tests/frame/frameProductsList.spec.js
+++ tests/frame/frameProductsList.spec.js
@@ -1,16 +1,16 @@
-import { test } from '@playwright/test';
+import { test, expect } from '@playwright/test';
 
-test('Products list can be opened within the frame', async ({}) => {
-  /*
-  Test:
-  1. Open the page
-   https://webdriveruniversity.com/IFrame/index.html
-  2. Click 'Our Products' link
-  3. Assert that all the block links are visible
-   ('Specifal offers',  'Cameras', etc.) 
+test('Products list can be opened within the frame', async ({page}) => {
+  let frame = page.frameLocator('#frame');
 
-  Tip:
-  Remember, that you need firstly to define the frame locator, 
-  and then find other elements within it. 
-  */
+  await page.goto('https://webdriveruniversity.com/IFrame/index.html');
+  await frame.getByRole('link', {name: 'Our Products'}).click();
+  await expect(frame.locator('#special-offers')).toBeVisible();
+  await expect(frame.locator('#cameras')).toBeVisible();
+  await expect(frame.locator('#new-laptops')).toBeVisible();
+  await expect(frame.locator('#used-laptops')).toBeVisible();
+  await expect(frame.locator('#game-consoles')).toBeVisible();
+  await expect(frame.locator('#components')).toBeVisible();
+  await expect(frame.locator('#desktop-systems')).toBeVisible();
+  await expect(frame.locator('#audio')).toBeVisible();
 });
