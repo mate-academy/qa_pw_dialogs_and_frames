dc1bda67e09ee8cdbe4d3a2e7c0b9f04bdf7724f
VoloGor <goralvv@gmail.com>
2026-06-26 15:09:40 +0200

    Work with Dialogs and Frames

--- tests/dialogs/promptDialog.spec.js
+++ tests/dialogs/promptDialog.spec.js
@@ -1,6 +1,6 @@
-import { test } from '@playwright/test';
+import { test, expect } from '@playwright/test';
 
-test('Prompt dialog message saves provided input', async ({}) => {
+test('Prompt dialog message saves provided input', async ({ page }) => {
   /*
   Test:
   1. Open the page
@@ -14,4 +14,26 @@ test('Prompt dialog message saves provided input', async ({}) => {
   8. Assert the dialog message is 'I prompt you'
   9. Assert the message 'You clicked OK. 'prompt' returned ${value}' is visible
   */
+ let dialogMessage;
+  let dialogType;
+
+  await page.goto(
+    'https://testpages.eviltester.com/styled/alerts/alert-test.html',
+  );
+
+  page.on('dialog', async (dialog) => {
+    expect(dialog.type()).toBe('prompt');
+    dialogMessage = dialog.message();
+    dialogType =
+      dialog.type().charAt(0).toUpperCase() + dialog.type().slice(1);
+    await dialog.accept('AQA Student');
+  });
+
+  await page.locator('#promptexample').click();
+  expect(dialogType).toBe('Prompt');
+  expect(dialogMessage).toBe('I prompt you');
+
+  await expect(
+    page.getByText('You clicked OK. \'prompt\''),
+  ).toBeVisible();
 });
